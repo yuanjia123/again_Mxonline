eda1a4485cc0676a1284c512633f904af1120cd1
yuanjia123 <871105356@qq.com>
2020-10-22 17:00:37 +0800

    setting当中的context_processors处理器、是全局的、所有模板都可以使用、用他去判断、是否登录、如果登录、显示登录信息、 否则显示未登录

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
-#数据库查询
-from django.contrib.auth import authenticate
+#数据库查询   以及cook的设置，登录完成以后存在login当中
+from django.contrib.auth import authenticate,login
 #跳转
 from django.http import HttpResponseRedirect
-
+#跳转
 from django.urls import reverse
 
 # Create your views here.
@@ -19,7 +19,9 @@ class LoginView(View):
         #验证用户名、密码是否存在
         user = authenticate(username = user_name,password=password)
         if user is not None:
-            # 如果登录成功 跳转到主页
+            # 如果登录成功、记录登录信息
+            login(request,user)
+            #跳转到主页
             return HttpResponseRedirect(reverse('index'))
         else:
             return render(request,"login.html",{"msg":"用户名或者密码错误"})
