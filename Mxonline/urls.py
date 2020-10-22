97a1672930fa4c8483e347ec4e84d66fc06e76dd
yuanjia123 <871105356@qq.com>
2020-10-22 16:24:17 +0800

    使用form、完成一半登录

--- Mxonline/urls.py
+++ Mxonline/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 import xadmin
 from django.views.generic import TemplateView
-
+from apps.users.views import LoginView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
-    path('login/', TemplateView.as_view(template_name="login.html"),name = "login"),  #其别名
+    path('', TemplateView.as_view(template_name="index.html"),name='index'),
+    path('login/', LoginView.as_view(),name = "login"),  #其别名
 
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+
+#数据库查询
+from django.contrib.auth import authenticate
+#跳转
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
 
 # Create your views here.
+class LoginView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'login.html')
+
+    def post(self,request,*args,**kwargs):
+        user_name = request.POST.get("username","")
+        password = request.POST.get("password","")
+        #验证用户名、密码是否存在
+        user = authenticate(username = user_name,password=password)
+        if user is not None:
+            # 跳转到主页
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request,"login.html",{"msg":"用户名或者密码错误"})
