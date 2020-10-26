1ca4db12353e6bc27d899043f699fe4af0339164
yuanjia123 <871105356@qq.com>
2020-10-26 09:32:49 +0800

    带账号、密码输入错误信息的业务逻辑

--- apps/users/views.py
+++ apps/users/views.py
@@ -16,12 +16,21 @@ class LoginView(View):
     def post(self,request,*args,**kwargs):
         user_name = request.POST.get("username","")
         password = request.POST.get("password","")
-        #验证用户名、密码是否存在
-        user = authenticate(username = user_name,password=password)
-        if user is not None:
-            # 如果登录成功、记录登录信息
-            login(request,user)
-            #跳转到主页
-            return HttpResponseRedirect(reverse('index'))
-        else:
-            return render(request,"login.html",{"msg":"用户名或者密码错误"})
+
+        #1、如果用户名不存在提示下面信息
+        if not user_name:
+            return render(request,"login.html",{"msg":"请输入用户名"})
+        #如果密码不存在
+        if not password:
+            return render(request,"login.html",{"msg":"请输入密码"})
+
+
+        # #验证用户名、密码是否存在
+        # user = authenticate(username = user_name,password=password)
+        # if user is not None:
+        #     # 如果登录成功、记录登录信息     调用login以后、就是一种登录的状态
+        #     login(request,user)
+        #     #跳转到主页
+        #     return HttpResponseRedirect(reverse('index'))
+        # else:
+        #     return render(request,"login.html",{"msg":"用户名或者密码错误"})
