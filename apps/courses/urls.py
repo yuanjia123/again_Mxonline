2b5b935926068dea5a14399faf705e5a087a674a
yuanjia123 <871105356@qq.com>
2021-01-07 22:11:20 +0800

    11-12播放视频完成、采用阿里云里面的OSS

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from apps.courses.views import CourseListView,CourseDetailView,CourseLessonView,CourseCommentsView
+from apps.courses.views import CourseListView,CourseDetailView,CourseLessonView,CourseCommentsView,VideoView
 
 
 
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^(?P<course_id>\d+)/$', CourseDetailView.as_view(), name="detail"),
     url(r'^(?P<course_id>\d+)/lesson/$', CourseLessonView.as_view(), name="lesson"),
     url(r'^(?P<course_id>\d+)/comments/$', CourseCommentsView.as_view(), name="comments"),
+    url(r'^(?P<course_id>\d+)/video/(?P<video_id>\d+)$', VideoView.as_view(), name="video"),
 
 ]
